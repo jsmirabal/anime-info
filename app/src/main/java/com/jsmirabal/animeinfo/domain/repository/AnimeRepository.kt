60b1d622a6c93ec6f736df994f0df25b14fc26b5
jsmirabal <julio.mirabal@globant.com>
2020-09-14 22:27:32 -0500

    Task(#30): Implement fetch most favorite animes use case

--- app/src/main/java/com/jsmirabal/animeinfo/domain/repository/AnimeRepository.kt
+++ app/src/main/java/com/jsmirabal/animeinfo/domain/repository/AnimeRepository.kt
@@ -22,6 +22,10 @@ interface AnimeRepository {
         page: String = NO_PAGE // Optional,
     ): ResultWrapper<DomainTopAnimes, DomainLayerError>
 
+    suspend fun fetchMostFavoriteAnimes(
+        page: String = NO_PAGE // Optional,
+    ): ResultWrapper<DomainTopAnimes, DomainLayerError>
+
     suspend fun fetchAnimeDetail(
         id: String
     ): ResultWrapper<DomainAnimeDetail, DomainLayerError>
